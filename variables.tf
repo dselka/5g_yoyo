9c856ac8a5859b0aa69f98682ec28ccf15839cf9
stiifii <stiifii@me.com>
2023-12-16 15:33:58 +0100

    fixed key

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-# Defining Public Key
-variable "public_key" {
-  default = "tests.pub"
-}
-# Defining Private Key
-variable "private_key" {
-  default = "tests.pem"
-}
 # Definign Key Name for connection
 variable "key_name" {
   default = "tests"
