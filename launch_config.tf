d6c481237e901601bdd28efc86f8f97b70fadb2e
stiifii <stiifii@me.com>
2023-12-16 15:16:23 +0100

    fixed typos

--- launch_config.tf
+++ launch_config.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "web" {
 image_id = "ami-087c17d1fe0178315" 
   instance_type = "t2.micro"
   key_name = "tests"
-security_groups = [ "${aws_security_group.demosg.id}" ]
+security_groups = [ "${aws_security_group.demosg2.id}" ]
   associate_public_ip_address = true
   user_data = "${file("data.sh")}"
 lifecycle {
--- provider.tf
+++ provider.tf
@@ -1,6 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-    region = "us-east-1"
-    access_key = "{}"
-    secret_key = "{}"
-    version = "~>5.0"
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
