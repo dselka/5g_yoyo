744b5a0f1c7ce13289024ae724e4d0de4c86fd4c
stiifii <stiifii@me.com>
2023-12-16 15:18:48 +0100

    aa

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = {}
+  secret_key = {}
 }
