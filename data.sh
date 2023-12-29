9c7d1e9961155851816cdfc2221b447c25725287
Your Name <you@example.com>
2023-12-30 00:40:43 +0100

    updated script to use apache plain app

--- data.sh
+++ data.sh
@@ -1,8 +1,30 @@
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
-sudo chmod 666 /var/run/docker.sock
-docker pull dhruvin30/dhsoniweb:v1
-docker run -d -p 80:80 dhruvin30/dhsoniweb:latest
+#!/bin/bash
+
+# Install Apache on Ubuntu
+sudo yum check-update
+sudo yum -y update
+# apache installation, enabling and status check
+sudo yum -y install httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl status httpd | grep Active
+# firewall installation, start and status check
+sudo yum install firewalld
+sudo systemctl start firewalld
+sudo systemctl status firewalld | grep Active
+# adding http services
+sudo firewall-cmd — permanent — add-service=http
+# reloading the firewall
+sudo firewall-cmd — reload
+
+
+sudo cat > /var/www/html/index.html << EOF
+<html>
+<head>
+  <title> Some ordinary application </title>
+</head>
+<body>
+  <p> I love Security of IOT </p>
+</body>
+</html>
+EOF
