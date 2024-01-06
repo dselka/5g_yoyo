a47b9c18c36c5b2cc415ffcc495e463c145c74d2
Your Name <you@example.com>
2024-01-06 21:07:28 +0100

    added an example python script

---
+++ skrypt.py
@@ -0,0 +1,20 @@
+import requests
+import threading
+import time
+import random
+
+
+def work():
+    while True:
+        liczba = 37
+        result = requests.get(f'http://web-elb-731190561.us-east-1.elb.amazonaws.com/?fiboIdx={liczba}')
+        if result.status_code == 200:
+            print('sent successfully')
+            time.sleep(random.randint(5, 10))
+        else:
+            print('pause')
+            time.sleep(20)
+
+for i in range(3):
+    thread = threading.Thread(target=work)
+    thread.start()
