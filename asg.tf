e47393ca044e235d9740979f340a4bff5632e42c
hubertDec <hubertdecyusz@gmail.com>
2024-01-06 16:22:36 +0100

    uncommented

--- asg.tf
+++ asg.tf
@@ -32,47 +32,47 @@ tag {
   }
 }
 
-# resource "aws_autoscaling_policy" "web_policy_up" {
-#   name = "web_policy_up"
-#   scaling_adjustment = 1
-#   adjustment_type = "ChangeInCapacity"
-#   cooldown = 60
-#   autoscaling_group_name = "${aws_autoscaling_group.web.name}"
-# }
-# resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
-#   alarm_name = "web_cpu_alarm_up"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods = "2"
-#   metric_name = "NetworkPacketsIn"
-#   namespace = "AWS/EC2"
-#   period = "300"
-#   statistic = "Average"
-#   threshold = "10000"
-# dimensions = {
-#     AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
-#   }
-# alarm_description = "This metric monitor EC2 instance CPU utilization"
-#   alarm_actions = [ "${aws_autoscaling_policy.web_policy_up.arn}" ]
-# }
-# resource "aws_autoscaling_policy" "web_policy_down" {
-#   name = "web_policy_down"
-#   scaling_adjustment = -1
-#   adjustment_type = "ChangeInCapacity"
-#   cooldown = 60
-#   autoscaling_group_name = "${aws_autoscaling_group.web.name}"
-# }
-# resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_down" {
-#   alarm_name = "web_cpu_alarm_down"
-#   comparison_operator = "LessThanOrEqualToThreshold"
-#   evaluation_periods = "2"
-#   metric_name = "NetworkPacketsIn"
-#   namespace = "AWS/EC2"
-#   period = "120"
-#   statistic = "Average"
-#   threshold = "1000"
-# dimensions = {
-#     AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
-#   }
-# alarm_description = "This metric monitor EC2 instance CPU utilization"
-#   alarm_actions = [ "${aws_autoscaling_policy.web_policy_down.arn}" ]
-# }
+ resource "aws_autoscaling_policy" "web_policy_up" {
+   name = "web_policy_up"
+   scaling_adjustment = 1
+   adjustment_type = "ChangeInCapacity"
+   cooldown = 60
+   autoscaling_group_name = "${aws_autoscaling_group.web.name}"
+ }
+ resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
+   alarm_name = "web_cpu_alarm_up"
+   comparison_operator = "GreaterThanOrEqualToThreshold"
+   evaluation_periods = "2"
+   metric_name = "NetworkPacketsIn"
+   namespace = "AWS/EC2"
+   period = "300"
+   statistic = "Average"
+   threshold = "10000"
+ dimensions = {
+     AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
+   }
+ alarm_description = "This metric monitor EC2 instance CPU utilization"
+   alarm_actions = [ "${aws_autoscaling_policy.web_policy_up.arn}" ]
+ }
+ resource "aws_autoscaling_policy" "web_policy_down" {
+   name = "web_policy_down"
+   scaling_adjustment = -1
+   adjustment_type = "ChangeInCapacity"
+   cooldown = 60
+   autoscaling_group_name = "${aws_autoscaling_group.web.name}"
+ }
+ resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_down" {
+   alarm_name = "web_cpu_alarm_down"
+   comparison_operator = "LessThanOrEqualToThreshold"
+   evaluation_periods = "2"
+   metric_name = "NetworkPacketsIn"
+   namespace = "AWS/EC2"
+   period = "120"
+   statistic = "Average"
+   threshold = "1000"
+ dimensions = {
+     AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
+   }
+ alarm_description = "This metric monitor EC2 instance CPU utilization"
+   alarm_actions = [ "${aws_autoscaling_policy.web_policy_down.arn}" ]
+ }
